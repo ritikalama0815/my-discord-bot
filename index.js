73c17609bc9e612caf5489679a8fb88f5e895649
Ritika Lama <ritika0815@MacBookAir.ht.home>
2025-03-11 20:24:10 -0500

    Update index.js to use .env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config();
 const { Client, Collection, Events, GatewayIntentBits, Message } = require('discord.js');
 const { token } = require('./config.json');
 
