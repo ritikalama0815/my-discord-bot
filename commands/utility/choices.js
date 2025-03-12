56d7a98d5bb284000d4f9a2761d9f972a0f546d1
Ritika Lama <ritika0815@MacBookAir.ht.home>
2025-03-11 21:53:54 -0500

    modified echo and gifs

--- commands/utility/choices.js
+++ commands/utility/choices.js
@@ -1,16 +1,37 @@
-const {SlashCommandBuilder} = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
-const data = new SlashCommandBuilder() 
-    .setName('gif')
-    .setDescription('Sends a random gif')
-    .addStringOption(option => 
-        option.setName('category')
-            .setDescription('What category of gif?')
-            .setRequired(true)
-            .addChoices(
-                { name: 'funny', value: 'funny' },
-                { name: 'sad', value: 'sad' },
-                { name: 'happy', value: 'happy' }
-            ));
-   
-    
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('gif')
+        .setDescription('Sends a random gif')
+        .addStringOption(option => 
+            option.setName('category')
+                .setDescription('What category of gif?')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'funny', value: 'funny' },
+                    { name: 'sad', value: 'sad' },
+                    { name: 'happy', value: 'happy' }
+                )),
+    async execute(interaction) {
+        const category = interaction.options.getString('category');
+        let gifUrl;
+
+        switch (category) {
+            case 'funny':
+                gifUrl = 'https://example.com/funny.gif';
+                break;
+            case 'sad':
+                gifUrl = 'https://example.com/sad.gif';
+                break;
+            case 'happy':
+                gifUrl = 'https://example.com/happy.gif';
+                break;
+            default:
+                gifUrl = 'https://example.com/default.gif';
+                break;
+        }
+
+        await interaction.reply({ content: gifUrl });
+    },
+};
--- commands/utility/echo.js
+++ commands/utility/echo.js
@@ -1,19 +1,47 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ChannelType } = require('discord.js');
 
-const data = new SlashCommandBuilder()
-    .setName('echo')
-    .setDescription('Replies with your input!')
-    .addStringOption(option => 
-        option.setName('input')
-            .setDescription('The input to echo back')
-            //to ensure that text will fit in a message
-            .setMaxLength(2_000)
-            .setRequired(true))
-    .addChannelOption(option =>
-        option.setName('channel')
-            .setDescription('The channel to send the message to')
-            .addChannelTypes(ChannelType.GuildText))
-    .addBooleanOption(option =>
-        option.setName('embed')
-            .setDescription('Whether or not echo should be embedded'));
-            
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('echo')
+        .setDescription('Replies with your input!')
+        .addStringOption(option => 
+            option.setName('input')
+                .setDescription('The input to echo back')
+                .setMaxLength(2_000)
+                .setRequired(true))
+        .addChannelOption(option =>
+            option.setName('channel')
+                .setDescription('The channel to send the message to')
+                .addChannelTypes(ChannelType.GuildText))
+        .addBooleanOption(option =>
+            option.setName('embed')
+                .setDescription('Whether or not echo should be embedded')),
+    async execute(interaction) {
+        const input = interaction.options.getString('input');
+        const channel = interaction.options.getChannel('channel');
+        const embed = interaction.options.getBoolean('embed');
+
+        if (channel) {
+            if (embed) {
+                const embedMessage = {
+                    color: 0x0099ff,
+                    description: input,
+                };
+                await channel.send({ embeds: [embedMessage] });
+            } else {
+                await channel.send(input);
+            }
+            await interaction.reply({ content: 'Message sent!', ephemeral: true });
+        } else {
+            if (embed) {
+                const embedMessage = {
+                    color: 0x0099ff,
+                    description: input,
+                };
+                await interaction.reply({ embeds: [embedMessage] });
+            } else {
+                await interaction.reply(input);
+            }
+        }
+    },
+};
